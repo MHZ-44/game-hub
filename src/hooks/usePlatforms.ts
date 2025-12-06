4d08f9751e829afe5c69b1aa8aa43d44ef32d6ea
Muhammad Hamzah Al-Masri <muhammad.hamzah.almasri@gmail.com>
2025-12-06 14:07:02 +0300

    Applying react query on platforms and add platforms data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,20 @@
-import useData from "./useData"
+import { useQuery } from "@tanstack/react-query"
+import { FetchRespone } from "./useData"
+import apiClient from "../services/api-client"
+import platforms from "../components/data/platforms"
 
 export interface Platform{
     id: number
     name: string
     slug: string
 }
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents")
+const usePlatforms = () => useQuery ({
+    queryKey: ['/platforms'],
+    queryFn: () => apiClient
+        .get<FetchRespone<Platform>>('/platforms/lists/parents')
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms }
+})
 
 export default usePlatforms
